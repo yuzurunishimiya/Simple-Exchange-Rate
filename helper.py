bd85d36d0c5aace8f8191bf7c1004c2c394bf72e
yuzurunishimiya <sonyalamsyah.learn@gmail.com>
2020-11-01 15:45:30 +0700

    helper: add limit and change print msg

--- helper.py
+++ helper.py
@@ -1,10 +1,13 @@
 def chooise_integer_strict(include=[], question=""):
     chooise = input(question)
+    if len(chooise) > 12:
+        print("\n\tCannot insert value that exceeds the limit (on 100 billion), Please try again\n")
+        return chooise_integer_strict(include=include, question=question)
     if chooise.isdigit() is False:
-        print("Pilihan tidak tersedia (harus dalam angka), silahkan ulangi")
+        print("\n\tOptions is not available, Try Again\n")
         return chooise_integer_strict(include=include, question=question)
     elif include != [] and int(chooise) not in include:
-        print("Pilihan tidak tersedia (harus dalam angka), silahkan ulangi")
+        print("\n\tOptions is not available, Try Again\n")
         return chooise_integer_strict(include=include, question=question)
     print("")
     return int(chooise)
@@ -13,7 +16,7 @@ def chooise_integer_strict(include=[], question=""):
 def value_formatting(data:float=0.0) -> str:
     data = str(data) if type(data) == float else str(float(data))
     sep_data = data.split(".")
-    digit, comma = sep_data[0], sep_data[1][:2]
+    digit, comma = sep_data[0], sep_data[1][:3]
     group = []
     while digit and digit[-1].isdigit():
         group.append(digit[-3:])
